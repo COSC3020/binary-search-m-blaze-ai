47accb80f51c32ef18ca959170fae98b36ae322e
M <mblazek2@uwyo.edu>
2025-02-17 06:09:14 +0000

    initial attempt at solving the problem, installed jsverify and added plagiarism statement

--- code.js
+++ code.js
@@ -1,3 +1,21 @@
 function binarySearch(list, element) {
+
+    let left = 0;
+    let right = list.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right)/2);
+
+        if (list[mid] == element){
+            return mid;
+        }
+        else if (list[mid] < element){
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
     return -1;
 }
